cb7fe79fe4047a5d5d045f3c51b20ed4e1a8eafd
MinhTrungLeNguyen <mtrungleng1309@gmail.com>
2021-06-27 19:09:42 +0700

    Add DoAn_Demo
    Em gửi nguyên bài (có cả Client & Server-chưa có phần định dạng)

---
+++ DoAnCK_Demo/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace ĐoAnCK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butClient = new System.Windows.Forms.Button();
+            this.butServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butClient
+            // 
+            this.butClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butClient.Location = new System.Drawing.Point(59, 16);
+            this.butClient.Name = "butClient";
+            this.butClient.Size = new System.Drawing.Size(153, 42);
+            this.butClient.TabIndex = 1;
+            this.butClient.Text = "Client";
+            this.butClient.UseVisualStyleBackColor = true;
+            this.butClient.Click += new System.EventHandler(this.butClient_Click);
+            // 
+            // butServer
+            // 
+            this.butServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butServer.Location = new System.Drawing.Point(59, 77);
+            this.butServer.Name = "butServer";
+            this.butServer.Size = new System.Drawing.Size(153, 40);
+            this.butServer.TabIndex = 2;
+            this.butServer.Text = "Server";
+            this.butServer.UseVisualStyleBackColor = true;
+            this.butServer.Click += new System.EventHandler(this.butServer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 139);
+            this.Controls.Add(this.butServer);
+            this.Controls.Add(this.butClient);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button butClient;
+        private System.Windows.Forms.Button butServer;
+    }
+}
+
---
+++ DoAnCK_Demo/FormServer.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace ĐoAnCK
+{
+    partial class FormServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormServer));
+            this.rtxtBefore = new System.Windows.Forms.RichTextBox();
+            this.rtxtAfter = new System.Windows.Forms.RichTextBox();
+            this.butListen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtxtBefore
+            // 
+            this.rtxtBefore.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtBefore.Location = new System.Drawing.Point(34, 67);
+            this.rtxtBefore.Name = "rtxtBefore";
+            this.rtxtBefore.Size = new System.Drawing.Size(638, 146);
+            this.rtxtBefore.TabIndex = 0;
+            this.rtxtBefore.Text = "";
+            // 
+            // rtxtAfter
+            // 
+            this.rtxtAfter.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtAfter.Location = new System.Drawing.Point(34, 248);
+            this.rtxtAfter.Name = "rtxtAfter";
+            this.rtxtAfter.Size = new System.Drawing.Size(638, 146);
+            this.rtxtAfter.TabIndex = 1;
+            this.rtxtAfter.Text = "";
+            // 
+            // butListen
+            // 
+            this.butListen.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butListen.Location = new System.Drawing.Point(369, 12);
+            this.butListen.Name = "butListen";
+            this.butListen.Size = new System.Drawing.Size(303, 34);
+            this.butListen.TabIndex = 2;
+            this.butListen.Text = "Lắng nghe";
+            this.butListen.UseVisualStyleBackColor = true;
+            this.butListen.Click += new System.EventHandler(this.butListen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Định dạng ban đầu:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(12, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sau khi định dạng:";
+            // 
+            // FormServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(699, 406);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.butListen);
+            this.Controls.Add(this.rtxtAfter);
+            this.Controls.Add(this.rtxtBefore);
+            this.Name = "FormServer";
+            this.Text = "FormServer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtBefore;
+        private System.Windows.Forms.RichTextBox rtxtAfter;
+        private System.Windows.Forms.Button butListen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
